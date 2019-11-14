9b38b4ca0802beb214b417f83f69217ec70a3cf7
Daan Verkooij <d@anverkooij.nl>
2019-11-14 17:00:03 +0100

    LP Generation test

--- Operations.py
+++ Operations.py
@@ -9,6 +9,40 @@ from math import *
 import numpy as np
 
 
+
 #Where to start...
 
 
+
+
+
+#generate LP file
+
+    #generate Objective, constraints
+    
+    #Make parameters binary as needed
+
+    #Write everything to LP file
+
+
+
+#=================================================
+            # Test for writing lp files
+#=================================================
+f = open("Test.lp","w+")
+f.write("max: X_1+2*X_2;\n\n")
+f.write("C1: X_1<=12;\n")
+f.write("C2: X_2<=13;\n")
+f.close() #Works in LP Solve!
+
+    
+#Write everything to file
+f = open("TestCPlex.lp","w+")
+f.write("Maximize objective:\n")
+f.write("X_1+2*X_2\n\n")
+f.write("Subject to:\n")
+f.write("C1: X_1<=12\n")
+f.write("C2: X_2<=13\n")
+f.close() #works in CPLex, probably
+    
+    
