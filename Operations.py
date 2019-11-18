557cabfbcde3a1e1b0d1bd4055d5bc4cb63f56b7
Daan Verkooij <d@anverkooij.nl>
2019-11-18 09:15:38 +0100

    Setup time func, elaboration on cplex file, some extra documentation

--- Operations.py
+++ Operations.py
@@ -9,43 +9,65 @@ from math import *
 import numpy as np
 
 
+def timeToMin(Time):
+    #Takes input and converts it to minutes.
+    #Input can be in several formats: hh:mm (5:15), hh (5), hh:mm am/pm (5:15am), hh am/pm
+    #spaces do not matter
+    converted=0
+    if Time.find('pm')!=-1:
+        #add 12 hours to time if pm
+        converted+=12*60
+        #Get rid of am and pm since they have been used
+        numTime=Time.strip('am')
+        numTime=numTime.strip('pm')
+        numTime=numTime.strip(' ')
+    if Time.find(':')!=-1:
+        # ':' is found. 
+        # split time in hh:mm format into seperate items
+        spl=numTime.split(':')
+        if len(spl) == 2:
+            converted+=int(spl[0])*60+int(spl[1])
+        elif len(spl) == 1:
+            converted+=int(spl[0])*60
+        else:
+            print('error in timeToMin split')
+            return "error in timeToMin split"
+    else: 
+        return int(numTime)*60
+    return converted
 
-#Where to start...
+#    Name, identifier/number, Passengers Pi, Arrival time, Departure time, form factor
+flight1 = ["JFK23", 1, 250, timeToMin("5pm"),timeToMin("7pm"),"A"]
+flight2 = ["JFK23", 2, 250, timeToMin("5pm"),timeToMin("7pm"),"B"]
+flights = np.array([flight1,flight2])
 
 
 
 
+#list for binary generation
+binlist=[]
 
-#generate LP file
+#Take input flights and generate LP file
+f = open("Firstiteration.lp","w+")    
 
-    #generate Objective, constraints
-    
-    #Make parameters binary as needed
-
-    #Write everything to LP file
-
-
-
-#=================================================
-            # Test for writing lp files
-#=================================================
-f = open("Test.lp","w+")
-f.write("max: X_1+2*X_2;\n\n")
-f.write("C1: X_1<=12;\n")
-f.write("C2: X_2<=13;\n")
-f.close() #Works in LP Solve!
-
-    
-#Write everything to file
-f = open("TestCPlex.lp","w+")
+#generate Objective
 f.write("Maximize objective:\n")
-f.write("X_1+2X_2\n
-f.write("\n")
+f.write("X_1+2X_2\n") #OBJECTIVE NEEDS TO BE CHANGED OBVIOUSLY
+f.write("\n")    
+
+#generate constraints
 f.write("Subject to:\n")
-f.write("X_1<=12\n")
-f.write("X_2<=13\n")
+for i in range(len(flights)):
+    f.write("X_"+flights[i][1]+"....\n")
+    binlist.append("X_"+flights[i][1])
+
 f.write("\n")
-f.write("End\n")
-f.close() #works in CPLex, probably
-    
-    
+#Make parameters binary as needed
+f.write("binary\n")
+
+for i in binlist:
+    f.write(i+" ")
+
+#write end file
+f.write("\n")
+f.write("end")
