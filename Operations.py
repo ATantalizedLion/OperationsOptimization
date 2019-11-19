e185bbc967f6021f0f9822bf3ffa4fa133c89ce2
Daan <d@anverkooij.nl>
2019-11-19 11:20:05 +0100

    Moar stuff

--- Operations.py
+++ Operations.py
@@ -36,30 +36,72 @@ def timeToMin(Time):
         return int(numTime)*60
     return converted
 
+def timeTo5Min(Time):
+    #Takes input and converts it to slot per 5.
+    #Input can be in several formats: hh:mm (5:15), hh (5), hh:mm am/pm (5:15am), hh am/pm
+    #spaces do not matter
+    converted=0
+    if Time.find('pm')!=-1:
+        #add 12 hours to time if pm
+        converted+=12*60
+        #Get rid of am and pm since they have been used
+        numTime=Time.strip('am')
+        numTime=numTime.strip('pm')
+        numTime=numTime.strip(' ')
+    if Time.find(':')!=-1:
+        # ':' is found. 
+        # split time in hh:mm format into seperate items
+        spl=numTime.split(':')
+        if len(spl) == 2:
+            converted+=int(spl[0])*60+int(spl[1])
+        elif len(spl) == 1:
+            converted+=int(spl[0])*60
+        else:
+            print('error in timeToMin split')
+            return "error in timeToMin split"
+    else: 
+        return int(numTime)*60
+    return int(converted/5)
+
 #    Name, identifier/number, Passengers Pi, Arrival time, Departure time, form factor
-flight1 = ["JFK23", 1, 250, timeToMin("5pm"),timeToMin("7pm"),"A"]
-flight2 = ["JFK23", 2, 250, timeToMin("5pm"),timeToMin("7pm"),"B"]
+flight1 = ["JFK23", 1, 250, timeTo5Min("5pm"),timeTo5Min("7pm"),"A"]
+flight2 = ["JFK23", 2, 250, timeTo5Min("5pm"),timeTo5Min("7pm"),"B"]
 flights = np.array([flight1,flight2])
 
+print("Update dataset")
 
+#add buffers
+print("Implement buffers")
 
 
 #list for binary generation
 binlist=[]
 
 #Take input flights and generate LP file
-f = open("Firstiteration.lp","w+")    
+f = open("FirstIteration.lp","w+")    
 
 #generate Objective
 f.write("Maximize objective:\n")
 f.write("X_1+2X_2\n") #OBJECTIVE NEEDS TO BE CHANGED OBVIOUSLY
 f.write("\n")    
+print("Implement objectives")
 
 #generate constraints
 f.write("Subject to:\n")
-for i in range(len(flights)):
-    f.write("X_"+flights[i][1]+"....\n")
-    binlist.append("X_"+flights[i][1])
+
+# Example for loop for constraints
+#for i in range(len(flights)):
+#    f.write("X_"+flights[i][1]+"....\n")
+#    binlist.append("X_"+flights[i][1])
+
+#Gate constraint 1: Domestic flight to dom gate
+print("Implement GC1")
+
+#Gate constrain 2: 
+print("Implement GC2")
+
+#Form factor constriant: (Compliance of a/c formfactor to bay/gate) 
+print("Implement FFC")
 
 f.write("\n")
 #Make parameters binary as needed
